e2cab699855968e0c7afd577127203092bd1a729
XTroyZe <xtroyze@gmail.com>
2020-08-05 22:28:38 +0100

    Updates

---
+++ precache-manifest.0fa0ff0bc6ed81b83db4f741da48cbb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d210f9bb0017a2ed757f8f9ea6f612e",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "cf37791c2817cccefd92",
+    "url": "/simple-todo/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "1256f451933b3d8e1e97",
+    "url": "/simple-todo/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "cf37791c2817cccefd92",
+    "url": "/simple-todo/static/js/2.a595987e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/simple-todo/static/js/2.a595987e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1256f451933b3d8e1e97",
+    "url": "/simple-todo/static/js/main.1a7f7f80.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
